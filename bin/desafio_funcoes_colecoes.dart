956405616ef74a47d7cec1d781f712dfce491b37
GUILHERME <guilherme.augusto@ecofood.com.br>
2021-08-21 09:39:54 -0300

    projeto criado

---
+++ bin/desafio_funcoes_colecoes.dart
@@ -0,0 +1,5 @@
+import 'package:desafio_funcoes_colecoes/desafio_funcoes_colecoes.dart' as desafio_funcoes_colecoes;
+
+void main(List<String> arguments) {
+  print('Hello world: ${desafio_funcoes_colecoes.calculate()}!');
+}
---
+++ test/desafio_funcoes_colecoes_test.dart
@@ -0,0 +1,8 @@
+import 'package:desafio_funcoes_colecoes/desafio_funcoes_colecoes.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
